0793df7e8a453cfa6fc1d8afab382fa92fe808e2
Marcin Kopatys <mrku1337@gmail.com>
2025-07-15 21:17:13 +0200

    implement style folder

--- index.php
+++ index.php
@@ -4,10 +4,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Calendar Project" />
     <title>PHP Calendar</title>
+    <link rel="stylesheet" href="/style.css" />
 </head>
 
 <body>
+
+    <header>
+        <h1>🗓️ Course Calendar<br> My Calendar Project</h1>
+    </header>
+
+    <div class="clock-container">
+        <div id="clock"></div>
+    </div>
 
 </body>
 
