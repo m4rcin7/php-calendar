84af5bcf0245caf59d87533ea818cb5fb6917743
Marcin Kopatys <mrku1337@gmail.com>
2025-07-20 18:15:49 +0200

    implement EDIT function in PHP

--- calendar.php
+++ calendar.php
@@ -12,12 +12,12 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && ($_POST['action'] ?? '') === "add")
     $start = $_POST['start_date'] ?? '';
     $end = $_POST['end_date'] ?? '';
 
-    if ($course && $instructor && $start && $end && $startTime && $endTime) {
+    if ($task && $task_desc && $start && $end) {
         $stmt = $conn->prepare(
-            'INSERT INTO appointments (task_name, task_description, start_date, end_date) 
+            'INSERT INTO tasks (task_name, task_description, start_date, end_date) 
              VALUES (?, ?, ?, ?, ?, ?)'
         );
-        $stmt->bind_param('ssssss', $task, $task_desc, $start, $end);
+        $stmt->bind_param('ssss', $task, $task_desc, $start, $end);
         $stmt->execute();
         $stmt->close();
 
@@ -26,5 +26,28 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && ($_POST['action'] ?? '') === "add")
     } else {
         header("Location: " . $_SERVER["PHP_SELF"] . "?error=1");
         exit;
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] === "POST" && ($_POST["action"] ?? '') === 'edit') {
+    $id = $_POST["event_id"] ?? null;
+    $task = trim($_POST["task_name"]);
+    $task_desc = trim($_POST["task_description"]);
+    $start = $_POST["start_date"] ?? '';
+    $end = $_POST["end_date"] ?? '';
+
+    if ($id && $task && $task_desc && $start && $end) {
+        $stmt = $conn->prepare(
+            "UPDATE tasks SET task_name = ?, task_description = ?,  start_date = ?, end_date = ? WHERE id = ?"
+        );
+        $stmt->bind_param("ssssi", $task, $task_desc, $start, $end, $id);
+        $stmt->execute();
+        $stmt->close();
+
+        header("Location: " . $_SERVER["PHP_SELF"] . "?success=2");
+        exit();
+    } else {
+        header("Location: " . $_SERVER["PHP_SELF"] . "?error=2");
+        exit();
     }
 }
