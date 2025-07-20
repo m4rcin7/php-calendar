54ea4480386ebe836ee69a3096a1f2721dc96934
Marcin Kopatys <mrku1337@gmail.com>
2025-07-20 19:59:21 +0200

    >php logic

--- calendar.php
+++ calendar.php
@@ -60,7 +60,44 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && ($_POST["action"] ?? '') === 'delet
         $stmt->bind_param("i", $id);
         $stmt->execute();
         $stmt->close();
-        header("Location: " . $_SERVER["PHP_SELF"] . "?sucess=3");
+        header("Location: " . $_SERVER["PHP_SELF"] . "?success=3");
         exit();
     }
 }
+
+if (isset($_GET["success"])) {
+    $successMsg = match ($_GET["success"]) {
+        '1' => "✅ Task added successfully!",
+        '2' => "🔄 Task updated successfully!",
+        '3' => "❌ Task deleted successfully!",
+        default => ''
+    };
+}
+
+if (isset($_GET["error"])) {
+    $errorMsg = '❗ Error occured. Check your input!';
+}
+
+$result = $conn->query("SELECT *FROM tasks");
+
+if ($result && $result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $start = new DateTime($row["start_date"]);
+        $end = new DateTime($row["end_date"]);
+
+        while ($start <= $end) {
+            $eventFromDB[] = [
+                'id' => $row["id"],
+                "title" => "{$row['task_name']} - {$row['task_description']}",
+                "date" => $start->format('Y-m-d'),
+                "start" => $row["start_date"],
+                "end" => $row['end_date'],
+            ];
+
+            $start->modify("+1 day");
+        }
+    }
+}
+
+
+$conn->close();
